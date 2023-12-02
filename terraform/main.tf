749b64f3a6c8f9c51fad18bc96c865d992b7ad63
Momina <149628506+momina-mk@users.noreply.github.com>
2023-12-02 12:00:53 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
  provider "aws" {
   region = "us-east-1"
-  AWS_ACCESS_KEY_ID = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  AWS_SECRET_ACCESS_KEY = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  acces_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
 }
 
 resource "aws_iam_role" "lambda_exec" {
