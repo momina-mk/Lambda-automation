09172272f80c6e457dbc3a7bccfa3446007baba1
Momina <149628506+momina-mk@users.noreply.github.com>
2023-12-02 12:03:26 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
  provider "aws" {
   region = "us-east-1"
-  acces_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
   secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
 }
 
