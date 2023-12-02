85437f6d5e5c3a75e3873e6fb47a7c32c593ad89
Momina <149628506+momina-mk@users.noreply.github.com>
2023-12-02 12:32:09 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
  provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_access_key
+ }
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_execution_role"
 
