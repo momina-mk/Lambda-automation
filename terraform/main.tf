b0d6cdee227e6166879c0c8f3161bda24bd65b2c
Momina <149628506+momina-mk@users.noreply.github.com>
2023-12-03 01:26:22 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
 provider "aws" {
   region = "us-east-1"
 }
