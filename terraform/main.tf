1ef25ac9a71e7891c03eeaecdf8b7d337e2d52d6
Momina <149628506+momina-mk@users.noreply.github.com>
2023-12-02 18:27:17 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
- provider "aws" {
+provider "aws" {
   region = "us-east-1"
- }
+}
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/nodejs/"
+  output_path = "${path.module}/index.zip"
+}
+
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_execution_role"
 
@@ -22,12 +28,9 @@ resource "aws_lambda_function" "myfunction" {
   function_name = "myfunction"
   handler      = "index.handler"
   runtime      = "nodejs14.x"
-  filename     = filebase64("index.zip")
-
-  source_code_hash = filebase64sha256("index.zip")
+  filename     = data.archive_file.zip_the_python_code.output_path
 
   role = aws_iam_role.lambda_exec.arn
 }
-
 
 
